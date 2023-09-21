2ae66bf75a54e1b906bd0375943a21a52b2aa3c0
prajendrrh <125906644+prajendrrh@users.noreply.github.com>
2023-09-21 12:04:20 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -50,7 +50,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'Amazing National Parks (PY)',
+        'displayName': 'Wonderful National Parks (PY)',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
